2447fb93bf9bd9735ebe1f08d7e8233acd103994
cabralcm <christophercabral@gmail.com>
2021-03-11 12:06:05 -0500

    update

--- src/trydjango/products/models.py
+++ src/trydjango/products/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Product(models.Model): #product class is inheriting methods from models.Model class
     title        = models.CharField(max_length=120) #max_length = REQUIRED
-    description  = models.TextField()
-    price        = models.TextField()
-    summary      = models.TextField(default="No free lunch! Just Dinner for two!")
+    description  = models.TextField(blank=True, null=True)
+    price        = models.DecimalField(max_digits=1000,decimal_places=2)
+    summary      = models.TextField(blank=False, null=False)
+    featured = models.BooleanField() # null=True, or default=True
 
 
 
